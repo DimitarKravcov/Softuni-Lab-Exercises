d226e8bef7f9d092710cbf039bc6da764ef886f1
DimitarKravcov <96549522+DimitarKravcov@users.noreply.github.com>
2022-10-21 00:57:26 +0300

    Add files via upload

---
+++ HomeW12.java
@@ -0,0 +1,12 @@
+
+public class HomeW12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char ch = 'a';
+		int ascii = ch;
+		
+		System.out.println("The ASCII Symbol of " + ch + " is :" + ascii );
+	}
+}
